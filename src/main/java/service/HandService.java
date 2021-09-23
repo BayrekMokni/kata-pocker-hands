1fd90f4b21af9a57285b3654c1ee701c5f97d938
Bayrek Mokni <mokni.mohamedbayrek@gmail.com>
2021-09-23 21:07:00 +0100

    [HAND_GAME_PROCESS] Implement Pair card calculator

--- src/main/java/service/HandService.java
+++ src/main/java/service/HandService.java
@@ -1,16 +1,27 @@
 package service;
 
+import javafx.util.Pair;
 import model.Card;
 import model.HandCombination;
 import service.calculator.HighCard;
 import service.calculator.ICardCombinationCalculator;
+import service.calculator.PairCard;
 
 import java.util.List;
 
 public class HandService {
 
     public HandCombination getBestCombination(List<Card> cardList) {
-        ICardCombinationCalculator calculator = new HighCard();
-        return calculator.execute(cardList);
+        HandCombination pairCardResult;
+        ICardCombinationCalculator pairCardCalculator;
+
+        pairCardCalculator = new PairCard();
+        pairCardResult = pairCardCalculator.execute(cardList);
+        if (pairCardResult.getBestCombination().size() == 2){
+            return pairCardResult;
+        }
+
+        pairCardCalculator = new HighCard();
+        return pairCardCalculator.execute(cardList);
     }
 }
--- src/test/java/service/HandServiceTest.java
+++ src/test/java/service/HandServiceTest.java
@@ -4,14 +4,13 @@ import model.Card;
 import model.HandCombination;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static model.Hand.HIGH_CARD;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static model.Hand.PAIR;
+import static org.junit.Assert.*;
 import static service.CardParserService.parseToCard;
 
 public class HandServiceTest {
@@ -20,7 +19,7 @@ public class HandServiceTest {
 
     @Test
     public void getBestCombinationShouldRespond() {
-        List<Card> cardList = new ArrayList<>();
+        List<Card> cardList = buildCardListEntries(Arrays.asList("2H", "3D", "5S", "9C", "KD"));
         HandCombination handCombination = handService.getBestCombination(cardList);
         assertNotNull(handCombination);
     }
@@ -35,6 +34,20 @@ public class HandServiceTest {
 
         assertEquals(expectedResult.getCardValue().getValue(), handCombination.getBestCombination().get(0).getCardValue().getValue());
         assertEquals(HIGH_CARD, handCombination.getHand());
+    }
+
+    @Test
+    public void getBestCombinationShouldReturnPairResult() {
+        final String KING_DIAMOND_CARD = "KD";
+        final String KING_HEART_CARD = "KH";
+        List<Card> expectResult = Arrays.asList(parseToCard(KING_DIAMOND_CARD), parseToCard(KING_HEART_CARD));
+        List<Card> cardListEntries = buildCardListEntries(Arrays.asList("2H", "3D", "5S", KING_HEART_CARD, KING_DIAMOND_CARD));
+
+        HandCombination handCombination = handService.getBestCombination(cardListEntries);
+
+        assertEquals(2, handCombination.getBestCombination().size());
+        assertEquals(PAIR, handCombination.getHand());
+        assertTrue(handCombination.getBestCombination().containsAll(expectResult));
     }
 
     private List<Card> buildCardListEntries(List<String> cards) {
